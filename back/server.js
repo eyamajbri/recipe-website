2b876bb10cfdbb041ee7a1d08934733e961b6258
eyamajbri <majbrieya@gmail.com>
2023-06-02 13:40:47 +0100

    user

--- back/server.js
+++ back/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const user= require('./models/user');
 const app = express();
 
 app.use(cors());
@@ -9,4 +11,18 @@ app.get('/message', (req, res) => {
 });
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`);
+    connectDB();
   });
+  function connectDB() {
+    mongoose
+        .connect("mongodb+srv://selimKetata:11072001@cluster0.viz2sz2.mongodb.net/", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.error("Error connecting to MongoDB:", err);
+        });
+}
