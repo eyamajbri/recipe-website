92a316cc240e729168b84d799b72af8397636fb0
eyamajbri <majbrieya@gmail.com>
2023-06-02 18:44:03 +0100

    login

--- back/models/Recipe.js
+++ back/models/Recipe.js
@@ -1,34 +1,36 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
+const recipeSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  nb_likes: {
+    type: Number,
+    default: 0,
+  }
+});
 
+const Recipe = mongoose.model( 'Recipe', recipeSchema);
 
-const Schema = mongoose.Schema
-
-
-const recipe = new mongoose.Schema({
-    id:Number,
-    title: String,
-    description: String,
-    images: [String],
-    nationality: String,
-    chef: String,
-    category: String,
-    likes: Number,
-    comments: [{ user: String, comment: String }],
-    ingredients: [String],
-    preparation: [String],
-    date: {
-      type: String,
-      default: () => {
-        const currentDate = new Date();
-        const day = String
-const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const year = currentDate.getFullYear();
-        return `${day}-${month}-${year}`;
-      }
-    }
-  });
-
-
-const RecipeModel = mongoose.model("recipe",recipe)
-
-module.exports = RecipeModel
+module.exports = Recipe;
--- back/server.js
+++ back/server.js
@@ -1,123 +1,207 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const User= require('./models/User');
-const Recipe=require('./models/Recipe')
+const User = require('./models/User');
+const Recipe = require('./models/Recipe');
 const morgan = require('morgan');
 const bcryptjs = require('bcryptjs');
 const createUserValidation = require('./Validation/createUserValidation');
-const app = express();
+const jwt = require('jsonwebtoken'); 
 
+const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/users",async (req,res)=>{
-    const users = await User.find()
-    res.json(users)
-})
-app.post("/recipes", async (req, res) => {
-    try {
-      const {
-        id,
-        title,
-        description,
-        images,
-        nationality,
-        chef,
-        category,
-        likes,
-        comments,
-        ingredients,
-        preparation,
-        date
-      } = req.body;
-  
-      const newRecipe = new Recipe({
-        id,
-        title,
-        description,
-        images,
-        nationality,
-        chef,
-        category,
-        likes,
-        comments,
-        ingredients,
-        preparation,
-        date
-      });
-  
-      const savedRecipe = await newRecipe.save();
-  
-      res.status(201).json(savedRecipe);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Server Error" });
-    }
-  });
-  
-app.post("/users", async (req, res) => {
-    try {
-      console.log(req.body);
-      const validation = createUserValidation.validate(req.body);
-      if (validation.error) {
-        return res.status(400).json({ error: validation.error.details });
-      }
-  
-      const { email, password, firstName, lastName, nationality, favorites, photo } = validation.value;
-  
-      let userAlreadyExist = await User.findOne({ email });
-      if (userAlreadyExist) {
-        return res.json({ msg: "User with this email already exists" });
-      }
-  
-      const salt = bcryptjs.genSaltSync(10);
-      const hashedPassword = bcryptjs.hashSync(password, salt);
-  
-      const user = new User({
-        email,
-        firstName,
-        lastName,
-        nationality,
-        password: hashedPassword,
-        favorites,
-        photo,
-      });
-      await user.save();
-  
-      res.status(201).json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Server Error" });
-    }
-  });
-  
 
-// ===========
+// Middleware to log requests
+app.use(morgan('dev'));
 
-app.listen(8000, () => {
-    console.log(`Server is running on port 8000.`);
-    connectDB();
-  });
-  function connectDB() {
-    mongoose
-        .connect("mongodb+srv://eyamajbri:eyam@cluster.lyjhbei.mongodb.net/", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        .then(() => {
-            console.log("Connected to MongoDB");
-        })
-        .catch((err) => {
-            console.error("Error connecting to MongoDB:", err);
-        });
+// Connect to MongoDB
+connectDB();
+
+// Connect to MongoDB function
+function connectDB() {
+  mongoose
+    .connect('mongodb+srv://eyamajbri:eyam@cluster.lyjhbei.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Connected to MongoDB');
+      // Start the server once connected to MongoDB
+      app.listen(8000, () => {
+        console.log(`Server is running on port 8000.`);
+      });
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
 }
-app.get("/recipes", async (req, res) => {
+
+// Middleware for authentication
+const requireAuth = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  // Check if the token exists
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, 'your-secret-key');
+
+    // Attach the user ID to the request object
+    req.userId = decoded.userId;
+
+    // Proceed to the next middleware
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+// Routes
+
+// Get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+// Create a new recipe
+app.post('/recipes', requireAuth, async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      ingredients,
+      category,
+      image,
+      nb_likes,
+    } = req.body;
+
+    // Access the logged-in user's ID from req.userId
+    const user = req.userId;
+
+    const newRecipe = new Recipe({
+      user,
+      title,
+      description,
+      ingredients,
+      category,
+      image,
+      nb_likes,
+    });
+
+    const savedRecipe = await newRecipe.save();
+
+    res.status(201).json(savedRecipe);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+// Create a new user
+app.post('/users', async (req, res) => {
+  try {
+    console.log(req.body);
+    const validation = createUserValidation.validate(req.body);
+    if (validation.error) {
+      return res.status(400).json({ error: validation.error.details });
+    }
+
+    const {
+      email,
+      password,
+      firstName,
+      lastName,
+      nationality,
+      favorites,
+      photo,
+    } = validation.value;
+
+    let userAlreadyExist = await User.findOne({ email });
+    if (userAlreadyExist) {
+      return res.json({ msg: 'User with this email already exists' });
+    }
+
+    const salt = bcryptjs.genSaltSync(10);
+    const hashedPassword = bcryptjs.hashSync(password, salt);
+
+    const user = new User({
+      email,
+      firstName,
+      lastName,
+      nationality,
+      password: hashedPassword,
+      favorites,
+      photo,
+    });
+    await user.save();
+
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+// Get all recipes
+app.get('/recipes', async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+// Login route
+app.post('/login', async (req, res) => {
     try {
-      const recipes = await Recipe.find();
-      res.json(recipes);
+      const { email, password } = req.body;
+  
+      // Find the user by email
+      const user = await User.findOne({ email });
+  
+      // Check if the user exists
+      if (!user) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Compare the provided password with the hashed password
+      const isPasswordValid = await bcryptjs.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+      }
+  
+      // Generate a JWT token
+      const token = jwt.sign({ userId: user._id }, 'your-secret-key');
+  
+      // Send the token in the response
+      res.json({ token });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: "Server Error" });
+      res.status(500).json({ error: 'Server Error' });
     }
   });
   
+
+// Protected route example
+app.get('/protected', requireAuth, (req, res) => {
+  // Access the logged-in user's ID from req.userId
+  const userId = req.userId;
+
+  // Handle the protected route logic
+  // ...
+
+  res.json({ message: 'Protected route accessed successfully' });
+});
