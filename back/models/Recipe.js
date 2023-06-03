80c59d4ccdaf66cd8d728a45a19a8a30d4eb3a71
selimketata <121767998+selimketata@users.noreply.github.com>
2023-06-03 01:01:10 +0100

    date

--- back/models/Recipe.js
+++ back/models/Recipe.js
@@ -28,7 +28,18 @@ const recipeSchema = new mongoose.Schema({
   nb_likes: {
     type: Number,
     default: 0,
+  },
+  date: {
+    type: String,
+    default: () => {
+      const currentDate = new Date();
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+      const year = currentDate.getFullYear();
+      return `${day}-${month}-${year}`;
+    }
   }
+  
 });
 
 const Recipe = mongoose.model( 'Recipe', recipeSchema);
