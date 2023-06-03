9e5adc1dccfe9cdab064fde95a25011b20da433a
selimketata <121767998+selimketata@users.noreply.github.com>
2023-06-03 01:22:43 +0100

    .

--- back/models/Recipe.js
+++ back/models/Recipe.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
+  email: {
+    type: String},
   name: {
     type: String,
     required: true,
