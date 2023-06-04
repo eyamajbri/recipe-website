dd969275eafd6222293145f57b81b28d96094405
eyamajbri <majbrieya@gmail.com>
2023-06-04 11:43:54 +0100

    profil recipes working

--- client/src/pages/Profil/Profil.jsx
+++ client/src/pages/Profil/Profil.jsx
@@ -1,64 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaHeart, FaComment } from 'react-icons/fa';
 import "./Profil.css";
 
 const Profil = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUserRecipes = async () => {
+      const email = localStorage.getItem("email");
+      try {
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:8000/recipes?email=${email}`);
+        const data = await response.json();
+        console.log("data", data);
+        const userRecipes = data.filter(recipe => recipe.email === email);
+        setRecipes(userRecipes);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserRecipes();
+  }, []);
+
   return (
     <div>
       <div className="container-product">
-    <div className="box-1" ></div>
-    <div className="box-2">
-      <div className="bio">
-        <h1> {localStorage.getItem("name")} {localStorage.getItem("last")} </h1>
-        <p>"an aspiring amateur chef who loves experimenting with flavors and enjoys sharing their culinary creations with others."</p>
-      </div>
-      <div/>
-      <hr />
-      <br />
-      <h2>recipe-ps</h2>
-      <div className="recipe-p">
-        <img src={require('./thai-food.jpg')} />
-        <div className="desc">
-          <ul>
-            <li>
-              Thai-style mussels
-            </li>
-            <li>
-              Thai
-            </li>
-          </ul>
-          <button className="btn">Details</button>
-      
-          <br/>
-          <br/>
-          < FaHeart/> 100&nbsp;&nbsp;&nbsp;<FaComment/> 20
+        <div className="box-1"></div>
+        <div className="box-2">
+          <div className="bio">
+            <h1>{localStorage.getItem("name")} {localStorage.getItem("last")}</h1>
+            <p>"An aspiring amateur chef who loves experimenting with flavors and enjoys sharing their culinary creations with others."</p>
+          </div>
+          <div />
+          <hr />
+          <br />
+          <h2>My recipes</h2>
+          {recipes.map(recipe => (
+            <div className="recipe-p" key={recipe._id}>
+            <img  class="recipeimg" src={`http://localhost:8000/uploads/${recipe.image}`} alt="Recipe Image" width="100px"/>
+              <div className="desc">
+                <ul>
+                  <li>{recipe.name}</li>
+                </ul>
+                <button className="btn">Details</button>
+                <br />
+                <br />
+                <FaHeart /> {recipe.nb_likes} &nbsp;&nbsp;&nbsp;<FaComment /> 0
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <div className="recipe-p">
-        < img src={require('./thai-food.jpg')} />
-        <div className="desc">
-          <ul>
-            <li>
-              Thai-style mussels
-            </li>
-            <li>
-              Thai
-            </li>
-          </ul>
-          <button className="btn">Details</button>
-      
-          <br/>
-          <br/>
-          < FaHeart/> 100&nbsp;&nbsp;&nbsp;<FaComment/> 20
-        </div>
+      <div className="profil-pic" style={{ backgroundImage: `url(http://localhost:8000/uploads/${localStorage.getItem('photo')})` }}>
       </div>
-    </div>
-</div>
-<div className="profil-pic" style={{backgroundImage: `url(http://localhost:8000/uploads/${localStorage.getItem('photo')})` }}>
-  
-</div>
-
-
     </div>
   )
 }
