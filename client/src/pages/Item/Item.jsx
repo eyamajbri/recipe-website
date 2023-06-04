eaac9a706ca4b41dc8bef5e7535d7beec085fc44
eya <eya.khalfallah@ensi-uma.tn>
2023-06-04 11:27:24 +0100

    iteeem cv

--- client/src/pages/Item/Item.jsx
+++ client/src/pages/Item/Item.jsx
@@ -2,7 +2,28 @@ import React from "react";
 import "./Item.css";
 
 function Item() {
-  return <div>Item</div>;
+  return (
+    <div className='item'>
+    <div className="form-container-it">
+      <div className="par12-it">
+        <div className="imgg">
+          <img src={require('./photo10.jpg')} alt="" />
+        </div>
+        <div className="descr">
+        <b>Recipe:</b> Garden Fresh Salad
+                <br />
+                <br />
+<b>Description:</b> <br /><br />This garden fresh salad is a delightful combination of crisp vegetables, tangy dressing, and a sprinkle of cheese. It's a light and healthy option packed with vitamins and flavors that will leave you feeling refreshed and satisfied.
+<br />
+                <br />
+<b>Category:</b> Main Courses
+<br /><br />
+<b>BY:</b> Yasmine Mahmoudi
+        </div>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default Item;
--- client/src/pages/Recipe/Recipe.jsx
+++ client/src/pages/Recipe/Recipe.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React , {useState,useEffect} from "react";
 import {BsHeart } from 'react-icons/bs'
 import { FaRegComment } from 'react-icons/fa';
 import "./Recipe.css";
 
 function Recipe() {
+  const [featuredImage, setFeaturedImage] = useState('');
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const images = [
+      require('./img/photo93.jpg'),
+      require('./img/photo17.jpg'),
+      require('./img/home22.jpg')
+    ];
+    let currentIndex = 0;
+
+    const interval = setInterval(() => {
+      setIsTransitioning(true);
+      currentIndex = (currentIndex + 1) % images.length;
+      setFeaturedImage(images[currentIndex]);
+    }, 5000);
+
+    const transitionTimeout = setTimeout(() => {
+      setIsTransitioning(false);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(transitionTimeout);
+    };
+  }, []);
   return ( 
     <div>
       <div className="container-recipe">
-        <div className="box-13">
-          <img src={require('./img/photo93.jpg')}  />
+      <div className={`box-13 ${isTransitioning ? 'transitioning' : ''}`}>
+          <img src={featuredImage} alt="Loading" />
             <div className="text-container-h">
                 <h1>DELICIOUS RECIPES <br />DAILY UPDATED</h1>
                 <p>Daily new Recipes and Cooking tips</p>
